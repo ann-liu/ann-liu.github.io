9fc3fe10ea28f2f510004b455161d3afafe28098
ann liu <33971145+ann-liu@users.noreply.github.com>
2025-05-08 23:18:12 -0700

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 const animationTimeMs: number = 11500;
-const laptopWidth: number = 1440;
-
-document.addEventListener('DOMContentLoaded', () => {
-  setInterval(() => {
-    const codeSvgs: HTMLElement[] = Array.from(
-      document.querySelectorAll('.code')
-    );
-    codeSvgs.forEach(code => {
-      code.parentNode?.replaceChild(code.cloneNode(true), code);
-    });
-  }, animationTimeMs);
-});
+setInterval(() => {
+  const codeSvgs: HTMLElement[] = Array.from(
+    document.querySelectorAll('.code > circle')
+  );
+  codeSvgs.forEach(code => {
+    code.parentNode?.replaceChild(code.cloneNode(true), code);
+  });
+}, animationTimeMs);
